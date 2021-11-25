bbad7eb16f56544e1e08c14608f82dcd6ff10763
vinigim <vigimenes21@gmail.com>
2021-11-25 17:47:53 +0000

    Pipeline Multi Maste

--- Wes-Desafio-Final-DEVOPS-main/Build-k8s-mult-master/0-k8s/0-terraform/main.tf
+++ Wes-Desafio-Final-DEVOPS-main/Build-k8s-mult-master/0-k8s/0-terraform/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 locals {
-  subs = concat([aws_subnet.wes_sub_tf_public_0.id], [aws_subnet.wes_sub_tf_public_1.id], [aws_subnet.wes_sub_tf_public_2.id])
+  subs = concat([aws_subnet.vini_sub_tf_public_0.id], [aws_subnet.vini_sub_tf_public_1.id], [aws_subnet.vini_sub_tf_public_2.id])
 }
 
 resource "aws_instance" "k8s_proxy" {
   ami = var.image_id
   depends_on = [
-    aws_subnet.wes_sub_tf_public_0, aws_subnet.wes_sub_tf_public_1
+    aws_subnet.vini_sub_tf_public_0, aws_subnet.vini_sub_tf_public_1
   ]
   instance_type               = "t3.medium"
   key_name                    = "chave_key_vini_06"
@@ -26,7 +26,7 @@ resource "aws_instance" "k8s_proxy" {
 resource "aws_instance" "k8s_masters" {
   ami = var.image_id
   depends_on = [
-    aws_subnet.wes_sub_tf_public_0, aws_subnet.wes_sub_tf_public_1, aws_instance.k8s_workers
+    aws_subnet.vini_sub_tf_public_0, aws_subnet.vini_sub_tf_public_1, aws_instance.k8s_workers
   ]
   instance_type               = "t3.large"
   key_name                    = "chave_key_vini_06"
@@ -42,7 +42,7 @@ resource "aws_instance" "k8s_masters" {
 resource "aws_instance" "k8s_workers" {
   ami = var.image_id
   depends_on = [
-    aws_subnet.wes_sub_tf_public_0, aws_subnet.wes_sub_tf_public_1
+    aws_subnet.vini_sub_tf_public_0, aws_subnet.vini_sub_tf_public_1
   ]
   instance_type               = "t3.medium"
   key_name                    = "chave_key_vini_06"
@@ -65,14 +65,14 @@ output "k8s-masters" {
 output "output-k8s_workers" {
   value = [
     for key, item in aws_instance.k8s_workers :
-    "k8s-workers ${key + 1} - ${item.private_ip} - ssh -i ~/.ssh/weslley_itau_rsa -o ServerAliveInterval=60 -o StrictHostKeyChecking=no ubuntu@${item.public_dns} "
+    "k8s-workers ${key + 1} - ${item.private_ip} - ssh -i ~/.ssh/id_rsa -o ServerAliveInterval=60 -o StrictHostKeyChecking=no ubuntu@${item.public_dns} "
   ]
 }
 
 output "output-k8s_proxy" {
   value = [
     for key, item in aws_instance.k8s_proxy :
-    "k8s-proxy ${key + 1} - ${item.private_ip} - ssh -i ~/.ssh/weslley_itau_rsa -o ServerAliveInterval=60 -o StrictHostKeyChecking=no ubuntu@${item.public_dns} "
+    "k8s-proxy ${key + 1} - ${item.private_ip} - ssh -i ~/.ssh/id_rsa -o ServerAliveInterval=60 -o StrictHostKeyChecking=no ubuntu@${item.public_dns} "
   ]
 }
 
@@ -85,25 +85,25 @@ output "security-group-master" {
 }
 
 output "subnet_public_0" {
-  value = aws_subnet.wes_sub_tf_public_0.id
+  value = aws_subnet.vini_sub_tf_public_0.id
 }
 
 output "subnet_public_1" {
-  value = aws_subnet.wes_sub_tf_public_1.id
+  value = aws_subnet.vini_sub_tf_public_1.id
 }
 
 output "subnet_public_2" {
-  value = aws_subnet.wes_sub_tf_public_1.id
+  value = aws_subnet.vini_sub_tf_public_1.id
 }
 
 output "subnet_priv_0" {
-  value = aws_subnet.wes_sub_tf_priv_0.id
+  value = aws_subnet.vini_sub_tf_priv_0.id
 }
 
 output "subnet_priv_1" {
-  value = aws_subnet.wes_sub_tf_priv_1.id
+  value = aws_subnet.vini_sub_tf_priv_1.id
 }
 
 output "subnet_priv_2" {
-  value = aws_subnet.wes_sub_tf_priv_2.id
+  value = aws_subnet.vini_sub_tf_priv_2.id
 }
