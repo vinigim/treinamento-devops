d33969df2685d1da17161b3ec214936b2fe47b46
vinigim <vigimenes21@gmail.com>
2021-11-25 18:22:11 +0000

    Pipeline Multi Maste

--- Wes-Desafio-Final-DEVOPS-main/Build-k8s-mult-master/0-k8s/0-terraform/subnet_priv.tf
+++ Wes-Desafio-Final-DEVOPS-main/Build-k8s-mult-master/0-k8s/0-terraform/subnet_priv.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "vini_sub_tf_priv_0" {
   vpc_id            = var.vpc_id
-  cidr_block        = "172.20.112.0/20"
+  cidr_block        = "172.80.10.0/24"
   availability_zone = "sa-east-1a"
 
   tags = {
@@ -10,7 +10,7 @@ resource "aws_subnet" "vini_sub_tf_priv_0" {
 
 resource "aws_subnet" "vini_sub_tf_priv_1" {
   vpc_id            = var.vpc_id
-  cidr_block        = "172.20.144.0/20"
+  cidr_block        = "172.80.20.0/24"
   availability_zone = "sa-east-1b"
 
   tags = {
@@ -20,7 +20,7 @@ resource "aws_subnet" "vini_sub_tf_priv_1" {
 
 resource "aws_subnet" "vini_sub_tf_priv_2" {
   vpc_id            = var.vpc_id
-  cidr_block        = "172.20.96.0/20"
+  cidr_block        = "172.80.30.0/24"
   availability_zone = "sa-east-1c"
 
   tags = {
