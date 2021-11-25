24c1c4837dee9f35fbc499a2eb4c800baff6de58
vinigim <vigimenes21@gmail.com>
2021-11-25 20:12:02 +0000

    Pipeline mysql

--- Wes-Desafio-Final-DEVOPS-main/Create-EC2_mysql/configura_mysql.sh
+++ Wes-Desafio-Final-DEVOPS-main/Create-EC2_mysql/configura_mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd Create-EC2_mysql/ansible
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts mysql.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/weslley_itau_rsa
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts mysql.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
