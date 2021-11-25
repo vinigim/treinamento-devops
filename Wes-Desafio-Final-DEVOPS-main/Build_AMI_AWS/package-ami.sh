a6c885ea585bd270f86686f6815c7f805242549c
vinigim <vigimenes21@gmail.com>
2021-11-25 15:56:20 +0000

    retirada do remote

--- Wes-Desafio-Final-DEVOPS-main/Build_AMI_AWS/package-ami.sh
+++ Wes-Desafio-Final-DEVOPS-main/Build_AMI_AWS/package-ami.sh
@@ -2,7 +2,7 @@
 
 VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
 
-cd ..terraform 
+cd terraform 
 RESOURCE_ID=$(terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
 
 cd ../terraform-ami
