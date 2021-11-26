565d1ef53a0894a9bf9606c5d4946b9e7f1593dc
vinigim <vigimenes21@gmail.com>
2021-11-26 12:42:23 +0000

    commit final

--- Wes-Desafio-Final-DEVOPS-main/Delivery_and_Deployment_Java_app/build_docker.sh
+++ Wes-Desafio-Final-DEVOPS-main/Delivery_and_Deployment_Java_app/build_docker.sh
@@ -5,7 +5,7 @@
 # docker build -t "didox/crud-java-login:$tag" -f Dockerfile .
 
 #### Pegando ultima tag do git #####
-cd Wes-Desafio-Final-DEVOPS-main
+
 tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo "Fazendo deploy da tag $tag"
-sudo docker build -t "vinigim/crud-java-login:$tag" -f Delivery_and_Deployment_Java_app/Dockerfile .
+sudo docker build -t "vinigim/crud-java-login:$tag" -f Wes-Desafio-Final-DEVOPS-main/Delivery_and_Deployment_Java_app/Dockerfile .
